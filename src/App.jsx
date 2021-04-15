827974627a0f1a2dca23a08a49e10d0c7082d23a
calvinhyacinth@gmail.com <calvinhyacinth@gmail.com>
2021-04-15 10:50:15 +0200

    work in progress on the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +22,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <Footer>
+    <div className="footer">
+      <div className="Row">
+        <div className="column">
+          <h1>Rate Us</h1>
+        </div>
+        <div className="column">
+          <h1>About Us</h1>
+          <h4><a href="https://www.linkedin.com/in/allan-caplain-79019a159/" target="_blank" rel="noreferrer">Allan Caplain</a></h4>
+          <h4><a href="https://www.linkedin.com/in/bastien-le-brun-3417b4137/" target="_blank" rel="noreferrer">Bastian Le Brun</a></h4>
+          <h4><a href="https://www.linkedin.com/in/calvinhyacinthmsc/" target="_blank" rel="noreferrer">Calvin Hyacinth</a></h4>
+          <h4><a href="https://www.linkedin.com/in/c%C3%A9dric-guyot-17231b209/" target="_blank" rel="noreferrer">Cédric Guyot</a></h4>
+          <h4><a href="https://www.linkedin.com/in/cyril-david-928403176/" target="_blank" rel="noreferrer">Cyril David</a></h4>
+        </div>
+        <div className="column">
+          <h1>Le bouton bonne conscience</h1>
+        </div>
+      </div>
+    </div>
+  </Footer>
+);
+
+export default Footer;
