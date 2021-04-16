ea7cc3ec76c51a4859b5a4cd07e68a60ce4aaca8
AllanCaplain <79872711+AllanCaplain@users.noreply.github.com>
2021-04-16 11:14:04 +0200

    fixed merge issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Footer from './component/Footer/Footer';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer />
-
 import CocktailList from './component/CocktailList/CocktailList';
 import Header from './component/Header/Header';
 
@@ -38,7 +16,7 @@ function App() {
     <div>
       <Header />
       <CocktailList cocktails={cocktailList} />
-
+      <Footer />
     </div>
   );
 }
