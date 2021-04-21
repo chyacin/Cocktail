2124aac593c33a1e68e7aa971fc2166149599225
calvinhyacinth@gmail.com <calvinhyacinth@gmail.com>
2021-04-21 16:23:55 +0200

    filter to get the cocktails

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css';
 import Footer from './component/Footer/Footer';
 import CocktailList from './component/CocktailList/CocktailList';
 import Header from './component/Header/Header';
+import SearchBar from './component/SearchBar/SearchBar';
 
 function App() {
   const [cocktailList, setCocktailList] = useState([]);
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
     'abcdefghijklmnopqrstuvwxyz0123456789'.split('').forEach((letter) => {
       fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
@@ -18,7 +21,11 @@ function App() {
   return (
     <div>
       <Header />
-      <CocktailList cocktails={cocktailList} />
+      <SearchBar getQuery={(q) => setQuery(q)} />
+      <CocktailList cocktails={cocktailList.filter(
+        (cocktail) => cocktail.strDrink.toLowerCase().includes(query.toLowerCase()),
+      )}
+      />
       <Footer />
     </div>
   );
