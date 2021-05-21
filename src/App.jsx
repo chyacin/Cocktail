25b8a53ef30f740fd1dd684df64028aba73ea918
calvinhyacinth@gmail.com <calvinhyacinth@gmail.com>
2021-05-21 11:56:10 +0200

    changes made for the app to work

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     });
   }, []);
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <main>
         <Route path="/" exact>
